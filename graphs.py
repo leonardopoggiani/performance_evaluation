899a204923a171f0a17030223b9710b78f19260b
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2023-05-09 00:43:51 +0200

    fixup! Modified graphs

--- graphs.py
+++ graphs.py
@@ -2,85 +2,73 @@ import sqlite3
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
-import pprint
+import math
 import datetime
-<<<<<<< HEAD
 import numpy as np
-import scipy
 import statistics
-from math import sqrt
-=======
 import scipy
->>>>>>> fe0ffa8 (fixup! Dividing functions for checking checkpoints)
+import pprint
 
 # function to add value labels
 def addlabels(x,y):
-    for i in range(len(x)):
-        plt.text(i,y[i],y[i])
+  for i in range(len(x)):
+    plt.text(i,y[i],y[i])
 
-def confidence_interval(data, confidence=0.95):
-    """
-    Calculate the confidence interval for a given dataset and confidence level.
-    
-    Args:
-    - data: A list or array of numerical data.
-    - confidence: A float representing the desired confidence level. Default is 0.95.
-    
-    Returns:
-    A tuple containing the lower and upper bounds of the confidence interval.
-    """
-    n = len(data)
-    m = np.mean(data)
-    std_err = sem(data)
-    h = std_err * t.ppf((1 + confidence) / 2, n - 1)
-    return m - h, m + h
+def mean_confidence_interval(data, confidence=0.95):
+  a = 1.0 * np.array(data)
+  n = len(a)
+  m, se = np.mean(a), scipy.stats.sem(a)
+  h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+  return m, m-h, m+h
 
-# function to add value labels
-def addlabels(x,y):
-    for i in range(len(x)):
-        plt.text(i,y[i],y[i])
+# 95% confidence interval for z = 1.96
+# 99% confidence interval for z = 2.576
+def plot_confidence_interval(x, values, z=1.96, color='#2187bb', horizontal_line_width=0.25):
+  mean = statistics.mean(values)
+  stdev = statistics.stdev(values)
+
+  confidence_interval = z * stdev / math.sqrt(len(values))
+
+  left = x - horizontal_line_width / 2
+  top = mean - confidence_interval
+  right = x + horizontal_line_width / 2
+  bottom = mean + confidence_interval
+  plt.plot([x, x], [top, bottom], color=color)
+  plt.plot([left, right], [top, top], color=color)
+  plt.plot([left, right], [bottom, bottom], color=color)
+  plt.plot(x, mean, 'o', color='#f44336')
+
+  return mean, confidence_interval
 
 def graph_checkpoint_size(c):
-    # Get data from database
+  # Get data from database
   c.execute('SELECT timestamp, containers, size FROM checkpoint_sizes')
   data = c.fetchall()
 
   # Create dictionary to store data by numContainers
   container_data = {}
   for container in set([x[1] for x in data]):
-      container_data[container] = []
-  
+    container_data[container] = []
+
   # Populate dictionary
   for row in data:
-      container_data[row[1]].append((row[0], row[2]))
-  
+    container_data[row[1]].append((row[0], row[2]))
+
   # Calculate averages
   averages = {}
   for container, values in container_data.items():
-      sizes = [v[1] for v in values]
-      avg_size = sum(sizes) / len(sizes)
-      averages[container] = avg_size
+    sizes = [v[1] for v in values]
+    avg_size = sum(sizes) / len(sizes)
+    averages[container] = avg_size
 
-<<<<<<< HEAD
   averages = {}
 
   for key, value in container_data.items():
     size_sum = 0
-    time_sum = 0
     count = len(value)
     for item in value:
-        size_sum += item[1]
+      size_sum += item[1]
     averages[key] = (size_sum/count)
-=======
-
-  fig = plt.figure()
-  ax = fig.add_axes([0,0,1,1])  
-  ax.bar(averages.keys(), averages.values())
-  ax.set_xlabel('Number of containers')
-  ax.set_ylabel('Average (MB)')
-  ax.set_title('Average of sizes by number of containers')
-  plt.show()
->>>>>>> fe0ffa8 (fixup! Dividing functions for checking checkpoints)
 
   fig = plt.figure()
 
@@ -94,25 +82,25 @@ def graph_checkpoint_size(c):
   # Create a list of colors for each bar
   colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
 
+  # Create a list of x-coordinates that correspond to the sorted keys of the averages dictionary
+  x_coords = [i for i in range(len(names))]
+
   # Plot the bars
-  for i in names:
-    pprint.pprint(i)
-    plt.bar(i, values[i], color=colors[i%len(colors)], align='center', width=0.7, label=f'{i} container')
-    plot_confidence_interval(i, container_data[i])
+  for i, container in enumerate(container_data.items()):
+    data = [x[1] for x in container[1]]
+    plot_confidence_interval(x_coords[i], data, color=colors.pop(0))
+
+  # Set the x-axis tick labels to the sorted keys of the averages dictionary
+  plt.xticks(x_coords, names)
 
   # calling the function to add value labels
   formatted_values = list(np.around(np.array(values),2))
   addlabels(names, formatted_values)
 
-  default_x_ticks = range(len(names))
-  plt.xticks(default_x_ticks, names)
   plt.xlabel('Number of containers')
   plt.ylabel('Average checkpoint size (MB)')
   plt.title('Checkpoint sizes by number of containers')
-<<<<<<< HEAD
 
-=======
->>>>>>> fe0ffa8 (fixup! Dividing functions for checking checkpoints)
   fig.autofmt_xdate()
 
   filename = "checkpoint_sizes_averages.png"
@@ -121,12 +109,82 @@ def graph_checkpoint_size(c):
 
   # Save graph image to "fig" folder
   if not os.path.exists("fig/averages"):
-      os.makedirs("fig/averages")
+    os.makedirs("fig/averages")
 
   fig.savefig(f"fig/averages/{new_filename}")
 
-  return
+def graph_checkpoint_time(c): 
+  # Get data from database
+  c.execute('SELECT timestamp, containers, size FROM checkpoint_times')
+  data = c.fetchall()
 
+  # Create dictionary to store data by numContainers
+  container_data = {}
+  for container in set([x[1] for x in data]):
+    container_data[container] = []
+
+  # Populate dictionary
+  for row in data:
+    container_data[row[1]].append((row[0], row[2]))
+
+  # Calculate averages
+  averages = {}
+  for container, values in container_data.items():
+    sizes = [v[1] for v in values]
+    avg_size = sum(sizes) / len(sizes)
+    averages[container] = avg_size
+
+  averages = {}
+
+  for key, value in container_data.items():
+    size_sum = 0
+    count = len(value)
+    for item in value:
+      size_sum += item[1]
+    averages[key] = (size_sum/count)
+
+  fig = plt.figure()
+
+  names = list(averages.keys())
+  names.sort()
+  values = list(averages.values())
+
+  # Sort the averages dictionary by keys
+  averages = dict(sorted(averages.items()))
+
+  # Create a list of colors for each bar
+  colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+
+  # Create a list of x-coordinates that correspond to the sorted keys of the averages dictionary
+  x_coords = [i for i in range(len(names))]
+
+  # Plot the bars
+  for i, container in enumerate(container_data.items()):
+    data = [x[1] for x in container[1]]
+    plot_confidence_interval(x_coords[i], data, color=colors.pop(0))
+
+  # Set the x-axis tick labels to the sorted keys of the averages dictionary
+  plt.xticks(x_coords, names)
+
+  # calling the function to add value labels
+  formatted_values = list(np.around(np.array(values),2))
+  addlabels(names, formatted_values)
+
+  plt.xlabel('Number of containers')
+  plt.ylabel('Average checkpoint time (ms)')
+  plt.title('Checkpoint times by number of containers')
+
+  fig.autofmt_xdate()
+
+  filename = "checkpoint_times_averages.png"
+  timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+  new_filename = os.path.splitext(filename)[0] + "_" + timestamp + os.path.splitext(filename)[1]
+
+  # Save graph image to "fig" folder
+  if not os.path.exists("fig/averages"):
+    os.makedirs("fig/averages")
+
+  fig.savefig(f"fig/averages/{new_filename}")
 
 if __name__ == "__main__":
   print("Graphing data...")
@@ -137,7 +195,7 @@ if __name__ == "__main__":
 
   graph_checkpoint_size(c)
 
-  # graph_checkpoint_time(c)
+  graph_checkpoint_time(c)
 
   # Close database connection
   conn.close()
--- performance_evaluation.go
+++ performance_evaluation.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 
@@ -14,6 +16,7 @@ import (
 	migrationoperator "github.com/leonardopoggiani/live-migration-operator/controllers"
 	_ "github.com/mattn/go-sqlite3"
 
+	"github.com/docker/docker/client"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -279,94 +282,8 @@ func cleanUp(ctx context.Context, clientset *kubernetes.Clientset, pod *v1.Pod) 
 	}
 }
 
-func getTotalTime(clientset *kubernetes.Clientset, numContainers int) (time.Duration, error) {
-	// Define the Pod manifest
-	podManifest := []byte(fmt.Sprintf(
-		`apiVersion: v1
-		kind: Pod
-		metadata:
-		  name: test-pod-%d-containers
-		spec:
-		  containers:`,
-		numContainers))
-
-	// Add the specified number of containers to the Pod manifest
-	for i := 0; i < numContainers; i++ {
-		containerManifest := fmt.Sprintf(
-			`- name: container-%d
-			image: nginx`,
-			i)
-		podManifest = append(podManifest, []byte(containerManifest)...)
-	}
-
-	// Finish the Pod manifest
-	podManifest = append(podManifest, []byte(`
-  		restartPolicy: Never
-	`)...)
-
-	// Create the Pod
-	pod, err := clientset.CoreV1().Pods("default").Create(context.TODO(), &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: fmt.Sprintf("test-pod-%d-containers", numContainers),
-			Labels: map[string]string{
-				"app": "test",
-			},
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
-				{
-					Name:  "nginx",
-					Image: "nginx",
-				},
-			},
-		},
-	}, metav1.CreateOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-
-	fmt.Printf("Pod %s is ready\n", pod.Name)
-
-	LiveMigrationReconciler := migrationoperator.LiveMigrationReconciler{}
-	var containers []migrationoperator.Container
-
-	// Append the container ID and name for each container in each pod
-	pods, err := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
-
-	for _, pod := range pods.Items {
-		for _, containerStatus := range pod.Status.ContainerStatuses {
-			idParts := strings.Split(containerStatus.ContainerID, "//")
-			if len(idParts) < 2 {
-				return 0, nil
-			}
-			containerID := idParts[1]
-
-			container := migrationoperator.Container{
-				ID:   containerID,
-				Name: containerStatus.Name,
-			}
-			containers = append(containers, container)
-		}
-	}
-
-	// Get the start time of the checkpoint
-	start := time.Now()
-
-	err = LiveMigrationReconciler.CheckpointPodPipelined(containers, "default", pod.Name)
-	if err != nil {
-		return 0, err
-	}
-	_, err = LiveMigrationReconciler.BuildahRestorePipelined(context.TODO(), pod.Name)
-	if err != nil {
-		return 0, err
-	}
-
-	// Calculate the time taken for the checkpoint
-	elapsed := time.Since(start)
-
-	fmt.Printf("Checkpoint took %s\n", elapsed)
-
-	return 0, nil
+func getTotalTime(clientset *kubernetes.Clientset, numContainers int) {
+	// TODO: implement me
 }
 
 func getCheckpointTimeSequential(ctx context.Context, clientset *kubernetes.Clientset, numContainers int, db *sql.DB) {
@@ -503,150 +420,169 @@ func getCheckpointTimePipelined(ctx context.Context, clientset *kubernetes.Clien
 	cleanUp(ctx, clientset, pod)
 }
 
-func getRestoreTime(ctx context.Context) (time.Duration, error) {
+func getRestoreTime(ctx context.Context, clientset *kubernetes.Clientset, numContainers int, db *sql.DB) {
 	// Get the start time of the restore
+
+	pod := createContainers(ctx, numContainers, clientset)
+
 	LiveMigrationReconciler := migrationoperator.LiveMigrationReconciler{}
 
-	start := time.Now()
-
-	_, err := LiveMigrationReconciler.BuildahRestore(ctx, "")
+	err := LiveMigrationReconciler.WaitForContainerReady(fmt.Sprintf("test-pod-%d-containers", numContainers), "default", fmt.Sprintf("container-%d", numContainers-1), clientset)
 	if err != nil {
-		return 0, err
-	}
-	// Calculate the time taken for the restore
-	elapsed := time.Since(start)
-	fmt.Println("Elapsed sequential: ", elapsed)
-
-	start = time.Now()
-
-	_, err = LiveMigrationReconciler.BuildahRestorePipelined(ctx, "")
-	if err != nil {
-		return 0, err
-	}
-	// Calculate the time taken for the restore
-	elapsed = time.Since(start)
-	fmt.Println("Elapsed pipelined: ", elapsed)
-
-	start = time.Now()
-
-	_, err = LiveMigrationReconciler.BuildahRestoreParallelized(ctx, "")
-	if err != nil {
-		return 0, err
-	}
-	// Calculate the time taken for the restore
-	elapsed = time.Since(start)
-	fmt.Println("Elapsed parallel: ", elapsed)
-
-	return 0, nil
-}
-
-func getCheckpointImageRestoreSize(ctx context.Context, numContainers int) (int64, error) {
-	/*
-		config, err := rest.InClusterConfig()
-		if err != nil {
-			return 0, fmt.Errorf("failed to get in-cluster config: %v", err)
-		}
-
-		clientset, err := kubernetes.NewForConfig(config)
-		if err != nil {
-			return 0, fmt.Errorf("failed to create kubernetes client: %v", err)
-		}
-
-		podName := fmt.Sprintf("test-pod-%d-containers", numContainers)
-
-		pod, err := clientset.CoreV1().Pods("default").Get(ctx, podName, metav1.GetOptions{})
-		if err != nil {
-			return 0, fmt.Errorf("failed to get pod: %v", err)
-		}
-
-		containerID := pod.Status.ContainerStatuses[0].ContainerID
-
-		containerLogOptions := &v1.PodLogOptions{
-			Container: pod.Spec.Containers[0].Name,
-		}
-
-		logStream, err := clientset.CoreV1().Pods("default").GetLogs(podName, containerLogOptions).Stream(ctx)
-		if err != nil {
-			return 0, fmt.Errorf("failed to get log stream: %v", err)
-		}
-		defer func(logStream io.ReadCloser) {
-			err := logStream.Close()
-			if err != nil {
-				klog.Errorf("failed to close log stream: %v", err)
-			}
-		}(logStream)
-
-		logBytes, err := io.ReadAll(logStream)
-		if err != nil {
-			return 0, fmt.Errorf("failed to read log stream: %v", err)
-		}
-
-		return int64(len(logBytes)), nil
-
-	*/
-	return 0, nil
-}
-
-func getTimeDirectVsTriangularized(clientset *kubernetes.Clientset, numContainers int) {
-	// Define the Pod manifest
-	podManifest := []byte(fmt.Sprintf(
-		`apiVersion: v1
-		kind: Pod
-		metadata:
-		  name: test-pod-%d-containers
-		spec:
-		  containers:`,
-		numContainers))
-
-	// Add the specified number of containers to the Pod manifest
-	for i := 0; i < numContainers; i++ {
-		containerManifest := fmt.Sprintf(
-			`- name: container-%d
-			image: nginx`,
-			i)
-		podManifest = append(podManifest, []byte(containerManifest)...)
-	}
-
-	// Finish the Pod manifest
-	podManifest = append(podManifest, []byte(`
-  		restartPolicy: Never
-	`)...)
-
-	// Create the Pod
-	pod, err := clientset.CoreV1().Pods("default").Create(context.TODO(), &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: fmt.Sprintf("test-pod-%d-containers", numContainers),
-			Labels: map[string]string{
-				"app": "test",
-			},
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
-				{
-					Name:  "nginx",
-					Image: "nginx",
-				},
-			},
-		},
-	}, metav1.CreateOptions{})
-	if err != nil {
-		panic(err.Error())
+		cleanUp(ctx, clientset, pod)
+		fmt.Println(err.Error())
+		return
 	}
 
 	fmt.Printf("Pod %s is ready\n", pod.Name)
 
-	// triangularized
-	LiveMigrationReconciler := migrationoperator.LiveMigrationReconciler{}
-
+	// Create a slice of Container structs
 	var containers []migrationoperator.Container
 
 	// Append the container ID and name for each container in each pod
-	pods, err := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
+	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		fmt.Println(err.Error())
+		cleanUp(ctx, clientset, pod)
+		return
+	}
 
 	for _, pod := range pods.Items {
 		for _, containerStatus := range pod.Status.ContainerStatuses {
 			idParts := strings.Split(containerStatus.ContainerID, "//")
 			if len(idParts) < 2 {
+				fmt.Println("Malformed container ID")
+				return
+			}
+			containerID := idParts[1]
+
+			container := migrationoperator.Container{
+				ID:   containerID,
+				Name: containerStatus.Name,
+			}
+			containers = append(containers, container)
+		}
+	}
+
+	err = LiveMigrationReconciler.CheckpointPodPipelined(containers, "default", pod.Name)
+	if err != nil {
+		return
+	}
+
+	// create dummy file
+	_, err = os.Create("/tmp/checkpoints/checkpoints/dummy")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	start := time.Now()
+
+	_, err = LiveMigrationReconciler.BuildahRestore(ctx, "/tmp/checkpoints/checkpoints")
+	if err != nil {
+		return
+	}
+	// Calculate the time taken for the restore
+	elapsed := time.Since(start)
+	fmt.Println("Elapsed sequential: ", elapsed)
+
+	saveRestoreTimeToDB(db, int64(numContainers), elapsed, "sequential")
+
+	// eliminate docker image
+	for i := 0; i < numContainers; i++ {
+		buildahDeleteImage("localhost/leonardopoggiani/checkpoint-images:container-" + strconv.Itoa(i))
+	}
+
+	start = time.Now()
+
+	_, err = LiveMigrationReconciler.BuildahRestorePipelined(ctx, "/tmp/checkpoints/checkpoints")
+	if err != nil {
+		return
+	}
+	// Calculate the time taken for the restore
+	elapsed = time.Since(start)
+	fmt.Println("Elapsed pipelined: ", elapsed)
+
+	saveRestoreTimeToDB(db, int64(numContainers), elapsed, "pipelined")
+
+	// eliminate docker image
+	for i := 0; i < numContainers; i++ {
+		buildahDeleteImage("localhost/leonardopoggiani/checkpoint-images:container-" + strconv.Itoa(i))
+	}
+
+	start = time.Now()
+
+	_, err = LiveMigrationReconciler.BuildahRestoreParallelized(ctx, "/tmp/checkpoints/checkpoints")
+	if err != nil {
+		return
+	}
+	// Calculate the time taken for the restore
+	elapsed = time.Since(start)
+	fmt.Println("Elapsed parallel: ", elapsed)
+
+	saveRestoreTimeToDB(db, int64(numContainers), elapsed, "parallelized")
+
+	// eliminate docker image
+	for i := 0; i < numContainers; i++ {
+		buildahDeleteImage("localhost/leonardopoggiani/checkpoint-images:container-" + strconv.Itoa(i))
+	}
+}
+
+func saveRestoreTimeToDB(db *sql.DB, numContainers int64, elapsed time.Duration, checkpoint_type string) {
+	// Prepare SQL statement
+	stmt, err := db.Prepare("INSERT INTO restore_times (containers, elapsed, checkpoint_type) VALUES (?, ?, ?)")
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	// Execute statement
+	_, err = stmt.Exec(numContainers, elapsed.Milliseconds(), checkpoint_type)
+	if err != nil {
+		return
+	}
+}
+
+func buildahDeleteImage(imageName string) {
+	buildahCmd := exec.Command("sudo", "buildah", "rmi", imageName)
+	err := buildahCmd.Run()
+	if err != nil {
+		fmt.Println("Error removing image:", err)
+		return
+	}
+	fmt.Println("Image", imageName, "removed successfully.")
+}
+
+func getCheckpointImageRestoreSize(ctx context.Context, clientset *kubernetes.Clientset, numContainers int, db *sql.DB) {
+	pod := createContainers(ctx, numContainers, clientset)
+
+	LiveMigrationReconciler := migrationoperator.LiveMigrationReconciler{}
+
+	err := LiveMigrationReconciler.WaitForContainerReady(fmt.Sprintf("test-pod-%d-containers", numContainers), "default", fmt.Sprintf("container-%d", numContainers-1), clientset)
+	if err != nil {
+		cleanUp(ctx, clientset, pod)
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("Pod %s is ready\n", pod.Name)
+
+	// Create a slice of Container structs
+	var containers []migrationoperator.Container
+
+	// Append the container ID and name for each container in each pod
+	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		fmt.Println(err.Error())
+		cleanUp(ctx, clientset, pod)
+		return
+	}
+
+	for _, pod := range pods.Items {
+		for _, containerStatus := range pod.Status.ContainerStatuses {
+			idParts := strings.Split(containerStatus.ContainerID, "//")
+			if len(idParts) < 2 {
+				fmt.Println("Malformed container ID")
 				return
 			}
 			containerID := idParts[1]
@@ -661,10 +597,68 @@ func getTimeDirectVsTriangularized(clientset *kubernetes.Clientset, numContainer
 
 	err = LiveMigrationReconciler.CheckpointPodCrio(containers, "default", pod.Name)
 	if err != nil {
+		fmt.Println(err.Error())
+		cleanUp(ctx, clientset, pod)
 		return
 	}
 
-	// ...
+	directory := "/tmp/checkpoints/checkpoints"
+
+	// Create a new Docker client
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// eliminate docker image
+	for i := 0; i < numContainers; i++ {
+		// Get the image details
+		imageName := "localhost/leonardopoggiani/checkpoint-images:container-" + strconv.Itoa(i)
+		image, _, err := cli.ImageInspectWithRaw(context.Background(), imageName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Print the size in MB
+		sizeInMB := float64(image.Size/1000000) / (1024 * 1024)
+		fmt.Printf("The size of %s is %.2f MB.\n", directory, sizeInMB)
+		saveDockerSizeToDB(db, int64(numContainers), sizeInMB)
+	}
+
+	// delete checkpoints folder
+	if _, err := exec.Command("sudo", "rm", "-f", directory+"/*").Output(); err != nil {
+		cleanUp(ctx, clientset, pod)
+		fmt.Println(err.Error())
+		return
+	}
+
+	// check that checkpoints folder is empty
+	if output, err := exec.Command("sudo", "ls", directory).Output(); err != nil {
+		cleanUp(ctx, clientset, pod)
+		fmt.Println(err.Error())
+		return
+	} else {
+		fmt.Printf("Output: %s\n", output)
+	}
+}
+
+func saveDockerSizeToDB(db *sql.DB, numContainers int64, size float64) {
+	// Prepare SQL statement
+	stmt, err := db.Prepare("INSERT INTO docker_sizes (containers, size, checkpoint_type) VALUES (?, ?, ?)")
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	// Execute statement
+	_, err = stmt.Exec(numContainers, size)
+	if err != nil {
+		return
+	}
+}
+
+func getTimeDirectVsTriangularized(clientset *kubernetes.Clientset, numContainers int) {
+	// TODO: implement me
 }
 
 func main() {
@@ -691,12 +685,14 @@ func main() {
 		panic(err)
 	}
 
-	_, err = db.Exec("DELETE FROM checkpoint_sizes")
+	// Create table if it doesn't exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS restore_times (timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, containers INTEGER, elapsed FLOAT, checkpoint_type STRING)")
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = db.Exec("DELETE FROM checkpoint_times")
+	// Create table if it doesn't exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS docker_sizes (timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, containers INTEGER, elapsed FLOAT)")
 	if err != nil {
 		panic(err)
 	}
@@ -727,7 +723,7 @@ func main() {
 
 	containerCounts := []int{1, 2, 5, 10}
 	// 	containerCounts := []int{1, 2, 3, 5, 10}
-	repetitions := 5
+	repetitions := 20
 	//  repetitions := 20
 
 	for i := 0; i < repetitions; i++ {
@@ -743,4 +739,37 @@ func main() {
 			getCheckpointTimePipelined(ctx, clientset, numContainers, db)
 		}
 	}
+
+	for i := 0; i < repetitions; i++ {
+		fmt.Printf("Repetition %d\n", i)
+
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Size for %d containers\n", numContainers)
+			getCheckpointSizeSequential(ctx, clientset, numContainers, db)
+		}
+
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Time for %d containers\n", numContainers)
+			getCheckpointTimeSequential(ctx, clientset, numContainers, db)
+		}
+	}
+
+	for i := 0; i < repetitions; i++ {
+		fmt.Printf("Repetition %d\n", i)
+
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Size for %d containers\n", numContainers)
+			getRestoreTime(ctx, clientset, numContainers, db)
+		}
+	}
+
+	for i := 0; i < repetitions; i++ {
+		fmt.Printf("Repetition %d\n", i)
+
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Size for %d containers\n", numContainers)
+			getCheckpointImageRestoreSize(ctx, clientset, numContainers, db)
+		}
+	}
+
 }
